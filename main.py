f5df0857cf567b02c6dc7ec4c1d40e934a3d3ac0
Thanga Ayyanar <thangaayyanar@gmail.com>
2025-02-19 22:02:09 +0530

    Simple demo using htmx

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse,FileResponse
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="assets",html = True), name="assets")
+app.mount("/assets", StaticFiles(directory="assets",html = True), name="assets")
 
-@app.get("/api")
+@app.get("/")
+async def read_index():
+    return FileResponse('assets/index.html')
+
+def generate_html_response():
+    html_content = """
+            <h1>Look ma! HTML!</h1>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/sample", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    return generate_html_response()
+
